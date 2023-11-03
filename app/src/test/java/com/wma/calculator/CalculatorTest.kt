972eb7a2237d4cd3545a2d5e7a4d1739bad0c347
aimeennnn <muh.aiman21304@gmail.com>
2023-11-04 04:59:01 +0700

    Pemberian Komentar Pada Class CalculatorTest

--- app/src/test/java/com/wma/calculator/CalculatorTest.kt
+++ app/src/test/java/com/wma/calculator/CalculatorTest.kt
@@ -4,10 +4,10 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class CalculatorTest {
-    @Test
-    fun test() {
-        val calculator = Calculator()
-        val result = calculator.add(3, 5)
-        assertEquals(8, result, "Berhasil")
+    @Test // Ini adalah anotasi yang menandakan metode berikutnya adalah metode pengujian.
+    fun test() {   // Ini adalah metode pengujian utama.
+        val calculator = Calculator()  // Membuat instance dari kelas Calculator.
+        val result = calculator.add(3, 5)  // Memanggil metode add() untuk menghitung hasil.
+        assertEquals(8, result, "Berhasil")   // Memeriksa apakah hasil sesuai dengan yang diharapkan.
     }
 }
