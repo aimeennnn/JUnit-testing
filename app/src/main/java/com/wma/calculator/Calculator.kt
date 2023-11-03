cf3565aee44704a53c1fe5c7d7c548c340a699b1
aimeennnn <muh.aiman21304@gmail.com>
2023-11-04 04:25:05 +0700

    Ini Bagian File Pertama Kali

---
+++ app/src/main/java/com/wma/calculator/Calculator.kt
@@ -0,0 +1,5 @@
+package com.wma.calculator
+
+class Calculator {
+
+}
---
+++ app/src/test/java/com/wma/calculator/CalculatorTest.kt
@@ -0,0 +1,8 @@
+package com.wma.calculator
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class CalculatorTest {
+
+}
